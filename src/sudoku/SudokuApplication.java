e0dd1c1991d4ed9246ef755d8e8f34de081430c2
Syed Jawad <jawadsyed406@gmail.com>
2024-04-23 06:33:50 +0300

    Application Container/Entry Point (SudokuApplication, Main)

---
+++ src/sudoku/SudokuApplication.java
@@ -0,0 +1,24 @@
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        uiImpl = new UserInterfaceImpl(primaryStage);
+        try {
+            SudokuBuildLogic.build(uiImpl);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
